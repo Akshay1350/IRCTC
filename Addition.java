19675016f9e75d287d2f72ade054e6b1234cc8d0
Akshay <aks@gmail.com>
2021-02-18 09:21:53 +0000

    BUG-1134 Rectified

---
+++ Addition.java
@@ -0,0 +1,16 @@
+pubic class Addition{
+
+
+    static int sum(int x,int y){
+
+          return x+y;
+
+    }
+     
+    psvm(String[] agrs){
+
+
+      SOP("The sum is::"+sum(10,20);
+
+    }
+}
--- Demo.java
+++ Demo.java
@@ -1,3 +1,11 @@
 public class Demo{
 
+
+ public static void main(String[] args){
+
+        System.out.println("Hi");
+        int x=10;
+}
+
+
 }
